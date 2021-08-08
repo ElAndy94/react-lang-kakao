db25870b06e1e9320187d776a616a3f33d818628
andrewpApadmi <andrewp@apadmi.com>
2021-08-08 09:46:38 +0100

    adding react lang translations

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import "./App.css";
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t, il8n } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <nav
         style={{ width: "100%", padding: "2rem 0", backgroundColor: "gray" }}
       >
-        <button onClick={() => il8n.changeLanguage("en")}>English</button>
-        <button onClick={() => il8n.changeLanguage("kor")}>Korean</button>
+        <button onClick={() => i18n.changeLanguage("en")}>English</button>
+        <button onClick={() => i18n.changeLanguage("kor")}>Korean</button>
       </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{t("Welcome to React")}</p>
+        <p>{t("HomePage.Welcome")}</p>
+        <p>{t("HomePage.reactApplication")}</p>
       </header>
     </div>
   );
