d6dcc3109e151c67c7f6ad90ab03694ca54ed8c8
andrewpApadmi <andrewp@apadmi.com>
2021-08-08 09:35:50 +0100

    first commit for react lang

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, il8n } = useTranslation();
   return (
     <div className="App">
+      <nav
+        style={{ width: "100%", padding: "2rem 0", backgroundColor: "gray" }}
+      >
+        <button onClick={() => il8n.changeLanguage("en")}>English</button>
+        <button onClick={() => il8n.changeLanguage("kor")}>Korean</button>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{t("Welcome to React")}</p>
       </header>
     </div>
   );
